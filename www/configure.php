0ebad35e6e3d1de36644cd3bea9f10151d5ced12
Peter Meehan <meehanp2@tcd.ie>
2015-11-23 11:42:53 +0000

    default code maybe

---
+++ www/configure.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Generated from 'configure.php.in' on Thu Nov 19 05:28:28 EST 2015.
+ *
+ * This script reads the configuration from its configured path.
+ *
+ * Part of the DOMjudge Programming Contest Jury System and licenced
+ * under the GNU GPL. See README and COPYING for details.
+ */
+
+require_once('/opt/domjudge/domserver/etc/domserver-static.php');
+
+require_once(ETCDIR . '/domserver-config.php');
